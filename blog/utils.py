e12ba10e8d3d06392c30fb3e2031ec2d0fae3d79
Fei Tan <fei@argonemyth.com>
2014-03-02 12:58:34 +0400

    Forgot to add the utils.py, but it's not in good state.

---
+++ blog/utils.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+
+import urllib, urllib2
+from urlparse import urljoin
+import os
+
+
+# TODO - will put it in the settings later
+DOMAIN = 'http://www.argonemyth.me'
+
+def download_photo(image_src):
+    image_url = urljoin(DOMAIN, image_src)
+    print "Downloading Image: %s" % image_url 
+    image_name = image_src.split("/")[-1]
+    file_name = image_name.split('.')
+    thumb_name = u"%s_thumb.%s" % (file_name[0], file_name[1])
+    thumb_src = image_src.replace(image_name, thumb_name)
+    thumb_url = urljoin(DOMAIN, thumb_src)
+    print "Downloading Image Thumb: %s" % thumb_url
+
+    outpath = os.path.join(settings.MEDIA_ROOT,
+                          image_src.replace('/media/', ''))
+    outpath_thumb = os.path.join(settings.MEDIA_ROOT,
+                                 thumb_src.replace('/media/', ''))
+    # print "Image output path: %s" % outpath
+    # print "Image thumb output path: %s" % outpath_thumb
+    urllib.urlretrieve(image_url, outpath)
+    urllib.urlretrieve(thumb_url, outpath_thumb)
